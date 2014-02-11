62cbeee841c973f07f69bedaa11dce35bf913bd5
Doug <ddg625@cs.utexas.edu>
2014-02-11 16:14:07 -0600

    Fixed infinate loop in cache generation, fixed #16.  Updated unit tests, fixed #14.

--- ActualGenerator.py
+++ ActualGenerator.py
@@ -1,12 +1,14 @@
 import sys
 import json
 
-def openMovie(m) :
+def findMovie(m) :
+	assert (int(m) > 0)
 	x = 7 - len(m)
 	s = "/u/downing/cs/netflix/training_set/mv_"
 	for i in range(x) :
 		s += "0"
 	s += m + ".txt"
+	assert (len(s) == 49)
 	return s
 
 def findUser(m, u) :
@@ -15,28 +17,29 @@ def findUser(m, u) :
 	for line in f :
 		x = line.split(',')
 		if x[0] == u :
-			return x[1]
-
-def readFile(r) :
-	return (str(s)[:len(s) - 1] for s in r)
+			return float(x[1])
+	assert (False)
 
 def buildActual() :
-	m = ""
+	f = open("/u/downing/cs/netflix/probe.txt", encoding = "ISO-8859-1")
 	cache = {}
-	for x in (readFile(sys.stdin)) :
-		if (x[len(x) - 1] == ":") :
-			m = openMovie(x[:len(x) - 1])
+	m = ""
+	for line in f :
+		if line[len(line) - 2] == ":" :
+			m = line[:len(line) - 2]
 		else :
-			cache[str((m, x))] = findUser(m, x)
-	with open('ActualCache.json', 'w') as f:
-		json.dump(cache, f)
+			assert (m != "")
+			u = line[:len(line) - 1]
+			cache[(str((m, u)))] = findUser(findMovie(m), u)
+	return cache
+
 
 def buildUser() :
 	f = open("/u/downing/cs/netflix/movie_titles.txt", encoding = "ISO-8859-1")
 	cache = {}
 	for line in f :
 		x = line.split(",")
-		m = openMovie(x[0])
+		m = findMovie(x[0])
 		f2 = open(m, "r")
 		f2.readline()
 		for line2 in f2 :
@@ -54,7 +57,7 @@ def buildMovie () :
 	cache = {}
 	for line in f :
 		x = line.split(",")
-		m = openMovie(x[0])
+		m = findMovie(x[0])
 		f2 = open(m, "r")
 		f2.readline()
 		cache[x[0]] = [0,0]
@@ -66,26 +69,29 @@ def buildMovie () :
 			cache[key] = value[0]/value[1]
 	return cache
 
-def buildCache ():
-	movies = buildMovie()
-	users = buildUser()
-	with open('MovieCache.json', 'w') as f:
-		json.dump(movies, f)
-	with open('UserCache.json', 'w') as f:
-		json.dump(users, f)
+def buildCache (cache):
+	if cache == 0 or cache == 1 :
+		print ("MovieCache Started...")
+		movies = buildMovie()
+		with open('MovieCache.json', 'w') as f:
+			json.dump(movies, f)
+		print ("MovieCache Done.")
+	if cache == 0 or cache == 2 :
+		print ("UserCache Started...")
+		users = buildUser()
+		with open('UserCache.json', 'w') as f:
+			json.dump(users, f)
+		print ("UserCache Done.")
+	if cache == 0 or cache == 3 :
+		print ("ActualCache Started...")
+		actual = buildActual()
+		with open('ActualCache.json', 'w') as f:
+			json.dump(actual, f)
+		print ("ActualCache Done.")
 
-	"""	
-	OLD DECREPID CODE
-	combo = {}
-	m = 0
-	for x in (readFile(sys.stdin)) :
-		if (x[len(x) - 1] == ":") :
-			m = int(x[:len(x) - 1])
-		else :
-			assert(m > 0)
-			u = int(x)
-			combo[str((m, u))] = (movies[str(m)][0] / movies[str(m)][1] + users[str(u)][0] / users[str(u)][1]) / 2
-	with open('Cache.json', 'w') as f:
-		json.dump(combo, f)
-	"""
-buildActual()
+
+print (sys.argv[0])
+if len(sys.argv) > 1 :
+	buildCache(int(sys.argv[1]))
+else :
+	buildCache(0)
